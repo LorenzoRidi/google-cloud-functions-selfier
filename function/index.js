176f2539935da3783421a16dea4bc79e59a57893
Lorenzo Ridi <lorenzo.ridi@noovle.it>
2017-03-15 10:48:44 +0100

    Error handling on NL API calls

--- function/index.js
+++ function/index.js
@@ -36,94 +36,117 @@ exports.selfier = function selfier (req, res) {
 	busboy.on('finish', function() {
 		console.log('Done parsing form!');
 
-		var commentSentiment;
+		detectSentimentAndAnnotateImage(res, imageByteArray, comment, 1);
 
-		language.detectSentiment(comment).then(function(data) {
-			commentSentiment = data;
-
-			var base64Image = Buffer.concat(imageByteArray).toString('base64');
-
-			var annotateImageReq = {
-				'image':{'content':base64Image},
-				'features':[{'type':'FACE_DETECTION','maxResults':'3'}]
-			}
-
-			vision.annotate(annotateImageReq,function(err,annotations, apiResponse) {
-
-				jimp.read(Buffer.concat(imageByteArray), function(err, image) {
-					if(err) {
-						res.writeHead(500,{'Access-Control-Allow-Origin':'*'})
-						res.write(JSON.stringify(err));
-						res.end();
-						return;
-					}
-
-					if(commentSentiment[0] >= 0) {
-						image = image.normalize().brightness(0.2).contrast(0.2);
-					} else {
-						image = image.greyscale();
-					}
-
-					async.each(annotations[0].faceAnnotations, function(annotation, callback) {
-						if(annotation.joyLikelihood != "VERY_UNLIKELY") {
-							console.log("Someone seems joyful here.");
-							jimp.read("https://storage.googleapis.com/noovle-gcf-demo-static/img/sparkles.png", function(err, sunImage) {
-								if(err) {
-									res.writeHead(500,{'Access-Control-Allow-Origin':'*'})
-									res.write(JSON.stringify(err));
-									res.end();
-									return;
-								}
-
-								sunImage = sunImage.resize((annotation.fdBoundingPoly.vertices[1].x - annotation.fdBoundingPoly.vertices[0].x), jimp.AUTO);
-								image = image.composite(sunImage,annotation.fdBoundingPoly.vertices[0].x,annotation.boundingPoly.vertices[0].y - (sunImage.bitmap.height / 2));
-								callback();
-							})
-						} else if(annotation.angerLikelihood != "VERY_UNLIKELY") {
-							console.log("Someone seems angry here.");
-							jimp.read("https://storage.googleapis.com/noovle-gcf-demo-static/img/cloud.png", function(err, rainImage) {
-								if(err) {
-									res.writeHead(500,{'Access-Control-Allow-Origin':'*'})
-									res.write(JSON.stringify(err));
-									res.end();
-									return;
-								}
-								
-								rainImage = rainImage.resize((annotation.fdBoundingPoly.vertices[1].x - annotation.fdBoundingPoly.vertices[0].x), jimp.AUTO);
-								image = image.composite(rainImage,annotation.fdBoundingPoly.vertices[0].x,annotation.boundingPoly.vertices[0].y - (rainImage.bitmap.height / 2));
-								callback();
-							})
-						} else {
-							console.log("No facial expressions here.");
-							callback();
-						}
-					}, function(err) {
-						if(err) {
-							res.writeHead(500,{'Access-Control-Allow-Origin':'*'})
-							res.write(JSON.stringify(err));
-							res.end();
-							return;
-						}
-								
-						image = image.resize(512,jimp.AUTO);
-
-						image.getBase64(jimp.MIME_PNG, function(err, base64Response) {
-							if(err) {
-								res.writeHead(500,{'Access-Control-Allow-Origin':'*'})
-								res.write(JSON.stringify(err));
-								res.end();
-								return;
-							}
-
-							res.writeHead(200,{'Access-Control-Allow-Origin':'*'})
-							res.write(base64Response);
-							res.end();
-						});
-					});
-				});				
-			});
-		});
 	});
 
 	req.pipe(busboy);
 };
+
+
+
+function returnBase64Image(base64Image, res) {
+	res.writeHead(200,{'Access-Control-Allow-Origin':'*'})
+	res.write(base64Image);
+	res.end();
+}
+
+function returnError(err, res) {
+	res.writeHead(500,{'Access-Control-Allow-Origin':'*'})
+	res.write(JSON.stringify(err));
+	res.end();
+}
+
+function returnImage(image, res) {
+	image = image.resize(512,jimp.AUTO);
+
+	image.getBase64(jimp.MIME_PNG, function(err, base64Response) {
+		if(err) {
+			returnError(err, res);
+			return;
+		}
+
+		returnBase64Image(base64Response, res);
+	});
+}
+
+function annotateImage(res, imageByteArray, commentSentiment) {
+
+	var base64Image = Buffer.concat(imageByteArray).toString('base64');
+
+	var annotateImageReq = {
+		'image':{'content':base64Image},
+		'features':[{'type':'FACE_DETECTION','maxResults':'3'}]
+	}
+
+	vision.annotate(annotateImageReq,function(err,annotations, apiResponse) {
+
+		jimp.read(Buffer.concat(imageByteArray), function(err, image) {
+			if(err) {
+				returnError(err, res);
+				return;
+			}
+
+			if(commentSentiment[0] >= 0) {
+				image = image.normalize().brightness(0.2).contrast(0.2);
+			} else {
+				image = image.greyscale();
+			}
+
+			async.each(annotations[0].faceAnnotations, function(annotation, callback) {
+				if(annotation.joyLikelihood != "VERY_UNLIKELY") {
+					console.log("Someone seems joyful here.");
+					jimp.read("https://storage.googleapis.com/noovle-gcf-demo-static/img/sparkles.png", function(err, sunImage) {
+						if(err) {
+							returnError(err, res);
+							return;
+						}
+
+						sunImage = sunImage.resize((annotation.fdBoundingPoly.vertices[1].x - annotation.fdBoundingPoly.vertices[0].x), jimp.AUTO);
+						image = image.composite(sunImage,annotation.fdBoundingPoly.vertices[0].x,annotation.boundingPoly.vertices[0].y - (sunImage.bitmap.height / 2));
+						callback();
+					})
+				} else if(annotation.angerLikelihood != "VERY_UNLIKELY") {
+					console.log("Someone seems angry here.");
+					jimp.read("https://storage.googleapis.com/noovle-gcf-demo-static/img/cloud.png", function(err, rainImage) {
+						if(err) {
+							returnError(err, res);
+							return;
+						}
+						
+						rainImage = rainImage.resize((annotation.fdBoundingPoly.vertices[1].x - annotation.fdBoundingPoly.vertices[0].x), jimp.AUTO);
+						image = image.composite(rainImage,annotation.fdBoundingPoly.vertices[0].x,annotation.boundingPoly.vertices[0].y - (rainImage.bitmap.height / 2));
+						callback();
+					})
+				} else {
+					console.log("No facial expressions here.");
+					callback();
+				}
+			}, function(err) {
+				if(err) {
+					returnError(err, res);
+					return;
+				}
+						
+				returnImage(image, res);
+				return;
+			});
+		});				
+	});
+}
+
+function detectSentimentAndAnnotateImage(res, imageByteArray, comment, retry) {
+	language.detectSentiment(comment).then(function(data) {
+
+		annotateImage(res, imageByteArray, data);
+
+	}, function(reason) {
+
+		if(retry < 5) {
+			detectSentimentAndAnnotateImage(res, imageByteArray,comment, retry + 1);
+		} else {
+			returnError(reason);
+		}
+
+	});
+}
